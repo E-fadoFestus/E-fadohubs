43385c9889e849400ef654b5195d47ccba02571c
FESTUS DANIEL OKHAWERE <efadotechcti@gmail.com>
2026-09-22 17:02:25 +0100

    Update index.ts

--- api/opay/initialize/index.ts
+++ api/opay/initialize/index.ts
@@ -1,6 +1,4 @@
-
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
